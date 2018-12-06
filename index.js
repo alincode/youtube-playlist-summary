e2614397bf8dda66d3558ffda4a2ff85d7fc3bed
Ai-Lin Liou <alincode@gmail.com>
2018-12-06 15:57:13 +0800

    fixed test and update outdate module

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class PlaylistSummary {
 
   constructor(config) {
     debug('config', config);
-    this.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || config.GOOGLE_API_KEY;
+    this.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || config && config.GOOGLE_API_KEY;
     if (!this.GOOGLE_API_KEY) throw new Error('missing required Youtube API key');
     this.api = new API(this.GOOGLE_API_KEY);
     this.format = new Format();
